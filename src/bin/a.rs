3a1e1dca0f1be33efc04578a0cc59e73f7d63a29
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-11-08 00:25:22 +0900

    sを完全推定したとしてタスクに待たせて得する場合を考慮(seed0:2025)

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -31,8 +31,8 @@ fn main() {
     // タスクには依存関係があるので、始められるタスクを列挙したい
     // 有向グラフだと思って、入次数が0の点から始めるのがよさそう？
     // タスクが完了したらタスク頂点から伸びている他の頂点の入次数のそれぞれ-1
-    let mut g = vec![vec![]; n];
-    let mut indeg = vec![0; n];
+    let mut g = vec![vec![]; input.n];
+    let mut indeg = vec![0; input.n];
     for (u, v) in input.uv.iter() {
         g[u - 1].push(v - 1);
         indeg[v - 1] += 1;
@@ -41,12 +41,16 @@ fn main() {
     let mut startable_tasks = (0..n).filter(|x| indeg[*x] == 0).collect::<HashSet<_>>();
 
     // メンバーには割当タスクがあるかどうかの状態があるので、管理したい
-    let mut member_state = vec![0; m];
+    let mut member_state = vec![0; input.m];
+
+    // メンバーごとに割り当てられているタスクが完了するのに要する見積もり日数を管理したい
+    // これt_(i,j)を推定することになるのでは？
+    let mut member_require_days = vec![0; input.m];
 
     // 要求技能レベルについては、一旦考慮しないこととする
-    let s = [
-        vec![16, 1, 14, 9, 0, 5, 15, 6, 2, 0, 4, 6, 2, 4, 7],
+    let mut s = [
         vec![0, 7, 28, 1, 11, 10, 9, 7, 4, 1, 6, 4, 2, 12, 9],
+        vec![16, 1, 14, 9, 0, 5, 15, 6, 2, 0, 4, 6, 2, 4, 7],
         vec![3, 3, 3, 1, 5, 12, 25, 2, 7, 13, 4, 20, 12, 23, 6],
         vec![13, 11, 6, 3, 11, 27, 3, 9, 8, 5, 10, 4, 2, 2, 16],
         vec![6, 5, 2, 1, 6, 14, 2, 15, 14, 3, 20, 6, 5, 17, 0],
@@ -67,23 +71,22 @@ fn main() {
         vec![8, 14, 12, 4, 14, 2, 3, 7, 6, 23, 18, 8, 7, 2, 11],
     ];
 
+    for s_row in s.iter_mut() {
+        if k > s_row.len() {
+            let p = k - s_row.len();
+            for _ in 0..p {
+                s_row.push(5);
+            }
+        } else {
+            let m = s_row.len() - k;
+            for _ in 0..m {
+                s_row.pop();
+            }
+        }
+    }
+
     loop {
-        // メンバーに対して割り当てられるタスクをすべて割り当てる
-        let free_members = member_state
-            .iter()
-            .enumerate()
-            .filter(|(_, s)| **s == 0)
-            .map(|(idx, _)| idx)
-            .collect::<Vec<_>>();
-
-        // タスクを割り当てる
-        // let m = free_members.len().min(startable_tasks.len());
-        // let assign = free_members
-        //     .iter()
-        //     .zip(startable_tasks.clone())
-        //     .collect::<Vec<_>>();
-
-        let assign = assign_task(&input, &free_members, &startable_tasks, &s);
+        let assign = assign_task(&input, &startable_tasks, &mut member_require_days, &s);
 
         // printは一回にまとめた方が早くなる？
         print!("{}", assign.len());
@@ -112,6 +115,7 @@ fn main() {
                 }
             }
             member_state[finished_member - 1] = 0;
+            member_require_days[finished_member - 1] = 0;
         }
 
         // 技能レベルの予測値
@@ -131,14 +135,13 @@ fn main() {
 
 fn assign_task(
     input: &Input,
-    free_members: &[usize],
     startable_tasks: &HashSet<usize, RandomState>,
+    member_require_days: &mut Vec<i32>,
     estimated_s: &[Vec<i32>],
 ) -> Vec<(usize, usize)> {
     // 推定しているsを元に割り当てるタスクの割当結果を返す
     // starttable_taskに存在するタスクそれぞれについて、現在フリーなメンバーをw_(i,j)の昇順にソートする
-    // starttable_taskについて、w_(i,j)の合計が小さくなるようにタスクを割り当てる
-    // 将来的に小さくなるようにする（そのうち）
+    // starttable_taskについて、w_(i,j) + (取り組んでいるタスクが終わるまでの日数)の合計が小さくなるようにタスクを割り当てる
 
     let mut weight = vec![vec![0; input.m]; input.n];
     for (i, w_row) in weight.iter_mut().enumerate() {
@@ -146,15 +149,19 @@ fn assign_task(
             for k in 0..input.k {
                 *w += (input.d[i][k] - estimated_s[j][k]).max(0);
             }
+            *w = 1.max(*w);
         }
     }
 
-    let mut fm = free_members.to_vec();
+    let mut fm = (0..input.m).collect::<Vec<_>>();
     let mut assign = vec![];
     for task in startable_tasks.iter() {
-        fm.sort_by_key(|j| -weight[*task][*j]);
+        fm.sort_by_key(|j| -(weight[*task][*j] + member_require_days[*j]));
         if let Some(assigned_m) = fm.pop() {
-            assign.push((assigned_m, *task));
+            if member_require_days[assigned_m] == 0 {
+                member_require_days[assigned_m] = weight[*task][assigned_m];
+                assign.push((assigned_m, *task));
+            }
         }
     }
     assign
