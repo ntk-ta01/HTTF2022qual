5257071e0327b574870258ae052ac16fac5bac6d
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-11-08 11:05:41 +0900

    sを推定し始めた

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,5 +1,7 @@
 #![allow(clippy::needless_range_loop, clippy::many_single_char_names)]
+use im_rc::HashMap;
 use proconio::{input, source::line::LineSource};
+use rand::prelude::*;
 use std::{
     collections::{hash_map::RandomState, HashSet},
     io::{self, BufReader},
@@ -28,6 +30,8 @@ fn main() {
     }
     let input = Input { n, m, k, r, d, uv };
 
+    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(0);
+
     // タスクには依存関係があるので、始められるタスクを列挙したい
     // 有向グラフだと思って、入次数が0の点から始めるのがよさそう？
     // タスクが完了したらタスク頂点から伸びている他の頂点の入次数のそれぞれ-1
@@ -44,57 +48,20 @@ fn main() {
     let mut member_state = vec![0; input.m];
 
     // メンバーごとに割り当てられているタスクが完了するのに要する見積もり日数を管理したい
-    // これt_(i,j)を推定することになるのでは？
+    // マイナスなら想定より遅く終わっている
     let mut member_require_days = vec![0; input.m];
+    // これt_(i,j)を推定することになるのでは？
 
     // 要求技能レベルについては、一旦考慮しないこととする
-    let mut s = [
-        vec![0, 7, 28, 1, 11, 10, 9, 7, 4, 1, 6, 4, 2, 12, 9],
-        vec![16, 1, 14, 9, 0, 5, 15, 6, 2, 0, 4, 6, 2, 4, 7],
-        vec![3, 3, 3, 1, 5, 12, 25, 2, 7, 13, 4, 20, 12, 23, 6],
-        vec![13, 11, 6, 3, 11, 27, 3, 9, 8, 5, 10, 4, 2, 2, 16],
-        vec![6, 5, 2, 1, 6, 14, 2, 15, 14, 3, 20, 6, 5, 17, 0],
-        vec![3, 13, 18, 26, 22, 14, 14, 0, 10, 0, 16, 10, 17, 5, 8],
-        vec![18, 11, 8, 7, 7, 9, 15, 12, 28, 4, 4, 1, 0, 3, 8],
-        vec![1, 1, 4, 5, 9, 2, 5, 12, 5, 2, 7, 3, 3, 3, 2],
-        vec![6, 11, 0, 6, 5, 5, 8, 0, 18, 5, 8, 0, 7, 5, 5],
-        vec![19, 19, 6, 16, 9, 20, 13, 5, 14, 16, 9, 16, 5, 2, 12],
-        vec![8, 7, 4, 6, 14, 14, 3, 8, 3, 3, 27, 4, 6, 11, 10],
-        vec![1, 1, 0, 7, 1, 11, 8, 7, 22, 5, 18, 7, 10, 12, 21],
-        vec![12, 13, 1, 12, 13, 5, 25, 23, 22, 9, 23, 8, 3, 14, 21],
-        vec![3, 2, 14, 6, 15, 9, 3, 1, 16, 6, 7, 1, 6, 1, 1],
-        vec![1, 0, 7, 12, 1, 2, 0, 1, 2, 6, 4, 1, 14, 6, 16],
-        vec![0, 9, 4, 4, 1, 6, 7, 5, 1, 8, 14, 5, 15, 1, 1],
-        vec![2, 11, 9, 12, 11, 3, 4, 19, 5, 4, 9, 4, 18, 30, 27],
-        vec![8, 5, 1, 3, 26, 24, 7, 5, 5, 5, 18, 7, 15, 6, 2],
-        vec![5, 4, 1, 3, 3, 9, 0, 4, 1, 11, 4, 6, 10, 6, 6],
-        vec![8, 14, 12, 4, 14, 2, 3, 7, 6, 23, 18, 8, 7, 2, 11],
-    ];
-
-    for s_row in s.iter_mut() {
-        if k > s_row.len() {
-            let p = k - s_row.len();
-            for _ in 0..p {
-                s_row.push(5);
-            }
-        } else {
-            let m = s_row.len() - k;
-            for _ in 0..m {
-                s_row.pop();
-            }
+    let mut s = vec![vec![1; input.k]; input.m];
+    for i in 0..input.m {
+        let mut b = vec![0.0; input.k];
+        for j in 0..input.k {
+            b[j] = f64::abs(rng.sample(rand_distr::StandardNormal));
         }
-    }
-
-    let mut weight = vec![vec![0; input.m]; input.n];
-    for (i, w_row) in weight.iter_mut().enumerate() {
-        for (j, w) in w_row.iter_mut().enumerate() {
-            for k in 0..input.k {
-                *w += (input.d[i][k] - s[j][k]).max(0);
-            }
-            if *w != 0 {
-                *w += 3;
-            }
-            *w = 1.max(*w);
+        let mul = rng.gen_range(20.0, 60.0) / b.iter().map(|x| x * x).sum::<f64>().sqrt();
+        for j in 0..input.k {
+            s[i][j] = (b[j] * mul).round() as i32;
         }
     }
 
@@ -104,7 +71,7 @@ fn main() {
             &startable_tasks,
             &member_state,
             &mut member_require_days,
-            &weight,
+            &s,
         );
 
         // printは一回にまとめた方が早くなる？
@@ -118,9 +85,7 @@ fn main() {
         println!("{}", output);
 
         for r_day in member_require_days.iter_mut() {
-            if *r_day > 0 {
-                *r_day -= 1;
-            }
+            *r_day -= 1;
         }
 
         let (n, f) = {
@@ -139,18 +104,47 @@ fn main() {
                     startable_tasks.insert(*nb);
                 }
             }
-            member_state[finished_member - 1] = 0;
+
+            // s[finished_member - 1]に修正をかけたい
+            // let task = member_state[finished_member - 1]; d[task - 1]について、遅れ日数分を引いた能力については保証できる
+            // 実際にはそこまで悲観的でなくともよさそう？ もう少し技能レベルについて楽観的に見てもよい？
+            // なんかモデルが必要そう
+            let task = member_state[finished_member - 1] - 1;
+            for (required_skill, skill) in
+                input.d[task].iter().zip(s[finished_member - 1].iter_mut())
+            {
+                // 想定より早く終わっていたら+、想定より遅く終わっていたら-の値
+                // だと思ったが、これでは一度本来の技能よりも大きくなると想定よりも早く終わって技能が本来の技能よりも大きくなってしまう
+                let delay = member_require_days[finished_member - 1];
+                // *skill = (required_skill + delay / input.k as i32 / 2).max(*skill);
+                // ↑require_skillに足してskillと大きい方だと技能kごとには変化しない
+                // delayの大きさとrequired_skillの大きさに応じて、skillがいい感じに変化していくと嬉しい
+                // s_i,jの生成方法を真似するとよい？参考にする必要はありそう
+                // s_iの合計値はどれくらいになる？
+                // s_i,jは[0,60]くらい
+                // *skill = (required_skill + delay + *skill) / 2;
+                // ↑delayの絶対値が小さいほどrequired_skillに似ている
+                *skill = if delay == 0 {
+                    *required_skill
+                } else {
+                    (*required_skill + *skill - delay) / 2
+                };
+                // あとweightも変えなきゃいけないよな
+                // タスク遂行に何日かかったか、required_skill、現在の推定skill
+            }
+
             member_require_days[finished_member - 1] = 0;
+            member_state[finished_member - 1] = 0;
         }
 
         // 技能レベルの予測値
-        // for (index, line) in s.iter().enumerate() {
-        //     print!("#s {}", index + 1);
-        //     for predict in line {
-        //         print!(" {}", predict);
-        //     }
-        //     println!();
-        // }
+        for (index, line) in s.iter().enumerate() {
+            print!("#s {}", index + 1);
+            for predict in line {
+                print!(" {}", predict);
+            }
+            println!();
+        }
 
         if n == -1 {
             break;
@@ -163,7 +157,7 @@ fn assign_task(
     startable_tasks: &HashSet<usize, RandomState>,
     member_state: &[usize],
     member_require_days: &mut Vec<i32>,
-    weight: &[Vec<i32>],
+    s: &[Vec<i32>],
 ) -> Vec<(usize, usize)> {
     // 推定しているsを元に割り当てるタスクの割当結果を返す
     // starttable_taskに存在するタスクそれぞれについて、現在フリーなメンバーをw_(i,j)の昇順にソートする
@@ -171,11 +165,24 @@ fn assign_task(
 
     let mut fm = (0..input.m).collect::<Vec<_>>();
     let mut assign = vec![];
+    let mut weight: HashMap<usize, i32> = HashMap::new();
     for task in startable_tasks.iter() {
-        fm.sort_by_key(|j| -(weight[*task][*j] + member_require_days[*j]));
+        for j in fm.iter() {
+            let mut w = 0;
+            for k in 0..input.k {
+                w += (input.d[*task][k] - s[*j][k]).max(0);
+            }
+            if w != 0 {
+                w += 3;
+            }
+            w = 1.max(w);
+            let key = *task + *j * input.n;
+            weight.insert(key, w);
+        }
+        fm.sort_by_key(|j| -(weight[&(*task + *j * input.n)] + member_require_days[*j]));
         if let Some(assigned_m) = fm.pop() {
             if member_state[assigned_m] == 0 {
-                member_require_days[assigned_m] = weight[*task][assigned_m];
+                member_require_days[assigned_m] = weight[&(*task + assigned_m * input.n)];
                 assign.push((assigned_m, *task));
             }
         }
