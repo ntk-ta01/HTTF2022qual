a00af529321b9ad113d4df4630dc71ea45ce448e
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-11-10 00:33:32 +0900

    sを上手く推定できない(seed0: 65463)

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,8 @@
 #![allow(clippy::needless_range_loop, clippy::many_single_char_names)]
-use im_rc::HashMap;
 use proconio::{input, source::line::LineSource};
-use rand::prelude::*;
+// use rand::prelude::*;
 use std::{
-    collections::{hash_map::RandomState, HashSet},
+    collections::{hash_map::RandomState, HashMap, HashSet},
     io::{self, BufReader},
 };
 
@@ -30,7 +29,7 @@ fn main() {
     }
     let input = Input { n, m, k, r, d, uv };
 
-    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(0);
+    // let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(0);
 
     // タスクには依存関係があるので、始められるタスクを列挙したい
     // 有向グラフだと思って、入次数が0の点から始めるのがよさそう？
@@ -52,9 +51,7 @@ fn main() {
     // メンバーごとに割り当てられているタスクが完了するのに要する見積もり日数を管理したい
     // マイナスなら想定より遅く終わっている
     let mut member_require_days = vec![0; input.m];
-    // これt_(i,j)を推定することになるのでは？
-    // メンバーごとに過去に割り振られたタスクと、かかった日数を覚えておく？
-    // かかった日数の少ないタスクの要求技能レベルをメンバーjの技能だとする
+    // メンバーごとに過去に割り振られたタスクと、かかった日数を覚えておく
     let mut member_assigned_tasks = vec![vec![]; input.m];
     let mut task_time = vec![-1; input.n];
 
@@ -71,7 +68,10 @@ fn main() {
     //     }
     // }
 
+    // let mut day = 0;
     loop {
+        // day += 1;
+        // eprintln!("day: {}", day);
         let assign = assign_task(
             &input,
             &startable_tasks,
@@ -79,7 +79,6 @@ fn main() {
             &mut member_require_days,
             &s,
         );
-
         // printは一回にまとめた方が早くなる？
         let mut output = assign.len().to_string();
         for (a, b) in assign {
@@ -118,8 +117,6 @@ fn main() {
             }
 
             // s[finished_member - 1]に修正をかけたい
-            // let task = member_state[finished_member - 1]; d[task - 1]について、遅れ日数分を引いた能力については保証できる
-            // 実際にはそこまで悲観的でなくともよさそう？ もう少し技能レベルについて楽観的に見てもよい？
             // なんかモデルが必要そう
             let task = member_state[finished_member - 1] - 1;
             processing_tasks.remove(&task);
@@ -129,10 +126,7 @@ fn main() {
 
             // task_timeが小さいタスクのdに近くなるようにして、大きいタスクから遠くなるようなベクトルs_jにする
 
-            for (i, past_task) in member_assigned_tasks[finished_member - 1]
-                .iter()
-                .enumerate()
-            {
+            for past_task in member_assigned_tasks[finished_member - 1].iter() {
                 // 過去のタスクについて、かかった日数がわかる
                 // 現在の推定sからかかる日数を推定できる
                 // 過去の推定sからかかる日数を推定してある
@@ -140,7 +134,7 @@ fn main() {
                 // そうでなくとも(かかった日数 - 1)だけ引いた分はどの技能kについても保証できる
                 let pena = task_time[*past_task] - 1;
                 for (k, k_skill) in input.d[*past_task].iter().enumerate() {
-                    let g_skill = 0.max(*k_skill - pena * pena * pena).max(*k_skill);
+                    let g_skill = (*k_skill - pena * pena * pena).max(*k_skill);
                     s[finished_member - 1][k] = g_skill;
                 }
             }
@@ -188,7 +182,7 @@ fn assign_task(
 
     let mut fm = (0..input.m).collect::<Vec<_>>();
     let mut assign = vec![];
-    let mut weight: HashMap<usize, i32> = HashMap::new();
+    let mut weight = HashMap::new();
     for task in startable_tasks.iter() {
         for j in fm.iter() {
             let mut w = 0;
@@ -199,21 +193,18 @@ fn assign_task(
                 w += 3;
             }
             w = 1.max(w) - 1;
-            let key = *task + *j * input.n;
+            let key = (*task, *j);
             weight.insert(key, w);
         }
-        fm.sort_by_key(|j| -(weight[&(*task + *j * input.n)]));
-        let mut erase_m = 21;
-        for (i, assigned_m) in fm.iter().enumerate() {
-            if member_state[*assigned_m] == 0 {
-                member_require_days[*assigned_m] = weight[&(*task + *assigned_m * input.n)];
-                assign.push((*assigned_m, *task));
-                erase_m = i;
-                break;
+        fm.sort_by_key(|j| {
+            -(weight[&(*task, *j)] + member_require_days[*j])
+            // + if member_state[*j] > 0 { 50 } else { 0 })
+        });
+        if let Some(assigned_m) = fm.pop() {
+            if member_state[assigned_m] == 0 {
+                member_require_days[assigned_m] = weight[&(*task, assigned_m)];
+                assign.push((assigned_m, *task));
             }
-        }
-        if erase_m != 21 {
-            fm.remove(erase_m);
         }
     }
     assign
