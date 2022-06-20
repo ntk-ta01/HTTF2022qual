53a88a2dd5af4906bd6bad906cd614ef6f9ee2e1
ntk_ta01 <43851479+ntk-ta01@users.noreply.github.com>
2022-06-20 11:23:05 +0900

    update local_test.fish

--- local_test.fish
+++ local_test.fish
@@ -1,6 +1,6 @@
 set score 0
 set now 0
-cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./in/0000.txt > out.txt ^tmp
+cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./tools/in/0000.txt > out.txt ^tmp
 cat tmp | while read line
     set now $line
 end
@@ -8,7 +8,7 @@ echo 0 $now
 set score (math $score + $now)
 
 for val in (seq 1 9)
-  cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./in/000$val.txt >./out/000$val.txt ^tmp
+  cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./tools/in/000$val.txt >./out/000$val.txt ^tmp
   cat tmp | while read line
     set now $line
   end
@@ -16,14 +16,13 @@ for val in (seq 1 9)
   set score (math $score + $now)
 end
 
-for val in (seq 10 99)
-  cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./in/00$val.txt >./out/00$val.txt ^tmp
+for val in (seq 10 49)
+  cargo run --release -p tools --bin tester cargo run --release --bin future-contest-2022-qual-a < ./tools/in/00$val.txt >./out/00$val.txt ^tmp
   cat tmp | while read line
     set now $line
   end
   echo $val $now
   set score (math $score + $now)
 end
-set score (math $score / 2)
 echo score: $score
 rm tmp
